e55bf7220dcacdbd3fd5bbaebac4e5268a03ff70
igserdar <63535330+igserdar@users.noreply.github.com>
2020-09-02 17:39:11 +0300

    Update Router.js

--- navigation/Router.js
+++ navigation/Router.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 
-import { createRouter } from '@exponent/ex-navigation';
+import { createRouter } from '@expo/ex-navigation';
 
 import BlankScreen from '../screens/BlankScreen';
 import DrawerNavigationScreen from '../screens/DrawerNavigationScreen';
@@ -11,13 +11,16 @@ import MoviesScreen from '../screens/MoviesScreen';
 import SearchScreen from '../screens/SearchScreen';
 import TabNavigationScreen from '../screens/TabNavigationScreen';
 
-export default createRouter(() => ({
-  blank: () => BlankScreen,
-  drawerNavigation: () => DrawerNavigationScreen,
-  modal: () => ModalScreen,
-  movie: () => MovieScreen,
-  movies: () => MoviesScreen,
-  moviesList: () => MoviesListScreen,
-  search: () => SearchScreen,
-  tabNavigation: () => TabNavigationScreen,
-}), {ignoreSerializableWarnings: true});
+export default createRouter(
+  () => ({
+    blank: () => BlankScreen,
+    drawerNavigation: () => DrawerNavigationScreen,
+    modal: () => ModalScreen,
+    movie: () => MovieScreen,
+    movies: () => MoviesScreen,
+    moviesList: () => MoviesListScreen,
+    search: () => SearchScreen,
+    tabNavigation: () => TabNavigationScreen
+  }),
+  { ignoreSerializableWarnings: true }
+);
