dc16e74fefc3f46235139cbfd304ffddc2ea22b9
igserdar <63535330+igserdar@users.noreply.github.com>
2020-09-02 17:36:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import Exponent from 'exponent';
+import Expo from 'expo';
 import { Platform, View } from 'react-native';
-import { Ionicons } from '@exponent/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 import { Provider } from 'react-redux';
 import store from './state/store';
 
 import navigationContext from './navigation/CustomNavigationContext';
-import { NavigationProvider, StackNavigation } from '@exponent/ex-navigation';
+import { NavigationProvider, StackNavigation } from '@expo/ex-navigation';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: '#000'}}>
+      <View style={{ flex: 1, backgroundColor: '#000' }}>
         <Provider store={store}>
           <NavigationProvider context={navigationContext}>
             <StackNavigation
               id="root"
               navigatorUID="root"
-              initialRoute={Platform.OS === 'android' ? 'drawerNavigation' : 'tabNavigation'}
+              initialRoute={
+                Platform.OS === 'android' ? 'drawerNavigation' : 'tabNavigation'
+              }
             />
           </NavigationProvider>
         </Provider>
@@ -30,21 +32,21 @@ export default class App extends React.Component {
 
 class AppContainer extends React.Component {
   state = {
-    isLoaded: false,
-  }
+    isLoaded: false
+  };
 
   async componentWillMount() {
-    await Exponent.Font.loadAsync(Ionicons.font)
-    this.setState({isLoaded: true});
+    await Expo.Font.loadAsync(Ionicons.font);
+    this.setState({ isLoaded: true });
   }
 
   render() {
     if (!this.state.isLoaded) {
-      return <Exponent.Components.AppLoading />;
+      return <Expo.AppLoading />;
     }
 
     return <App />;
   }
 }
 
-Exponent.registerRootComponent(AppContainer);
+Expo.registerRootComponent(AppContainer);
