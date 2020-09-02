f0620e7d31586c95079138832ab06a6c1e573bf5
igserdar <63535330+igserdar@users.noreply.github.com>
2020-09-02 17:41:54 +0300

    Update MoviesScreen.js

--- screens/MoviesScreen.js
+++ screens/MoviesScreen.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import {
   RefreshControl,
   ScrollView,
@@ -6,14 +7,14 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Platform,
+  Platform
 } from 'react-native';
-import Exponent from 'exponent';
+import Expo from 'expo';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Swiper from 'react-native-swiper';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { withNavigation } from '@exponent/ex-navigation';
+import { withNavigation } from '@expo/ex-navigation';
 
 import * as moviesActions from '../state/moviesActions';
 import CardOne from '../components/CardOne';
@@ -25,12 +26,15 @@ import styles from './styles/Movies';
 class SearchButton extends Component {
   render() {
     return (
-      <TouchableOpacity onPress={this._openSearch} style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingRight: 15,
-      }}>
+      <TouchableOpacity
+        onPress={this._openSearch}
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingRight: 15
+        }}
+      >
         <Icon name="ios-search" color="#fff" size={28} />
       </TouchableOpacity>
     );
@@ -38,16 +42,16 @@ class SearchButton extends Component {
 
   _openSearch = () => {
     this.props.navigation.showModal('search', { title: 'Search' });
-  }
+  };
 }
 
 class Movies extends Component {
   static route = {
     navigationBar: {
       title: 'Movies',
-      renderRight: () => Platform.OS === 'ios' && <SearchButton />,
-    },
-  }
+      renderRight: () => Platform.OS === 'ios' && <SearchButton />
+    }
+  };
 
   constructor(props) {
     super(props);
@@ -93,83 +97,131 @@ class Movies extends Component {
 
   render() {
     const { nowPlayingMovies, popularMovies } = this.props;
-    const iconPlay = <Icon name="md-play" size={21} color="#9F9F9F" style={{ paddingLeft: 3, width: 22 }} />;
-    const iconTop = <Icon name="md-trending-up" size={21} color="#9F9F9F" style={{ width: 22 }} />;
-    const iconUp = <Icon name="md-recording" size={21} color="#9F9F9F" style={{ width: 22 }} />;
+    const iconPlay = (
+      <Icon
+        name="md-play"
+        size={21}
+        color="#9F9F9F"
+        style={{ paddingLeft: 3, width: 22 }}
+      />
+    );
+    const iconTop = (
+      <Icon
+        name="md-trending-up"
+        size={21}
+        color="#9F9F9F"
+        style={{ width: 22 }}
+      />
+    );
+    const iconUp = (
+      <Icon
+        name="md-recording"
+        size={21}
+        color="#9F9F9F"
+        style={{ width: 22 }}
+      />
+    );
     const topInset = this.props.route.getContentInsetsStyle().marginTop;
 
-    return (
-      this.state.isLoading ? <View style={styles.progressBar}><ProgressBar /></View> :
-      <ScrollView
-        style={[styles.container]}
-        contentInset={{top: topInset}}
-        contentOffset={{y: -topInset}}
-        refreshControl={
-          <RefreshControl
-            refreshing={this.state.isRefreshing}
-            onRefresh={this._onRefresh}
-            colors={['#EA0000']}
-            tintColor="white"
-            title="loading..."
-            titleColor="white"
-            progressBackgroundColor="white"
-          />
-        }>
-        <Swiper
-          autoplay
-          autoplayTimeout={4}
-          showsPagination={false}
-          height={248}>
-          {nowPlayingMovies.results.map(info => (
-            <CardOne key={info.id} info={info} viewMovie={this._viewMovie} />
-          ))}
-        </Swiper>
-        <View>
-          <View style={styles.listHeading}>
-            <Text style={styles.listHeadingLeft}>Popular</Text>
-            <TouchableOpacity>
-              <Text
-                style={styles.listHeadingRight}
-                onPress={this._viewMoviesList.bind(this, 'popular', 'Popular')}>
-                See all
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            {popularMovies.results.map(info => (
-              <CardTwo key={info.id} info={info} viewMovie={this._viewMovie} />
-            ))}
-          </ScrollView>
-
-          <View style={styles.browseList}>
-            <TouchableOpacity activeOpacity={0.7} onPress={this._viewMoviesList.bind(this, 'now_playing', 'Now Playing')}>
-              <View style={styles.browseListItem}>
-                {iconPlay}
-                <Text style={styles.browseListItemText}>
-                  Now Playing
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.7} onPress={this._viewMoviesList.bind(this, 'top_rated', 'Top Rated')}>
-              <View style={styles.browseListItem}>
-                {iconTop}
-                <Text style={styles.browseListItemText}>
-                  Top Rated
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity activeOpacity={0.7} onPress={this._viewMoviesList.bind(this, 'upcoming', 'Upcoming')}>
-              <View style={styles.browseListItem}>
-                {iconUp}
-                <Text style={styles.browseListItemText}>
-                  Upcoming
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
+    return this.state.isLoading
+      ? <View style={styles.progressBar}>
+          <ProgressBar />
         </View>
-      </ScrollView>
-    );
+      : <ScrollView
+          style={[styles.container]}
+          contentInset={{ top: topInset }}
+          contentOffset={{ y: -topInset }}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.isRefreshing}
+              onRefresh={this._onRefresh}
+              colors={['#EA0000']}
+              tintColor="white"
+              title="loading..."
+              titleColor="white"
+              progressBackgroundColor="white"
+            />
+          }
+        >
+          <Swiper
+            autoplay
+            autoplayTimeout={4}
+            showsPagination={false}
+            height={248}
+          >
+            {nowPlayingMovies.results.map(info =>
+              <CardOne key={info.id} info={info} viewMovie={this._viewMovie} />
+            )}
+          </Swiper>
+          <View>
+            <View style={styles.listHeading}>
+              <Text style={styles.listHeadingLeft}>Popular</Text>
+              <TouchableOpacity>
+                <Text
+                  style={styles.listHeadingRight}
+                  onPress={this._viewMoviesList.bind(
+                    this,
+                    'popular',
+                    'Popular'
+                  )}
+                >
+                  See all
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {popularMovies.results.map(info =>
+                <CardTwo
+                  key={info.id}
+                  info={info}
+                  viewMovie={this._viewMovie}
+                />
+              )}
+            </ScrollView>
+
+            <View style={styles.browseList}>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={this._viewMoviesList.bind(
+                  this,
+                  'now_playing',
+                  'Now Playing'
+                )}
+              >
+                <View style={styles.browseListItem}>
+                  {iconPlay}
+                  <Text style={styles.browseListItemText}>Now Playing</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={this._viewMoviesList.bind(
+                  this,
+                  'top_rated',
+                  'Top Rated'
+                )}
+              >
+                <View style={styles.browseListItem}>
+                  {iconTop}
+                  <Text style={styles.browseListItemText}>Top Rated</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={this._viewMoviesList.bind(
+                  this,
+                  'upcoming',
+                  'Upcoming'
+                )}
+              >
+                <View style={styles.browseListItem}>
+                  {iconUp}
+                  <Text style={styles.browseListItemText}>Upcoming</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>;
   }
 }
 
@@ -179,7 +231,7 @@ Movies.propTypes = {
   popularMovies: PropTypes.object.isRequired,
   navigator: PropTypes.object,
   navigation: PropTypes.object,
-  route: PropTypes.object,
+  route: PropTypes.object
 };
 
 function mapStateToProps(state, ownProps) {
