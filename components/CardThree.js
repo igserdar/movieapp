efdd9ad0c4758ed4d329683f1efc914e1f9832f9
igserdar <63535330+igserdar@users.noreply.github.com>
2020-09-02 17:32:01 +0300

    Update CardThree.js

--- components/CardThree.js
+++ components/CardThree.js
@@ -1,13 +1,8 @@
+import PropTypes from 'prop-types';
 /* eslint-disable new-cap */
-import React, { PropTypes, Component } from 'react';
-import {
-  Image,
-  Platform,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import FadeIn from '@exponent/react-native-fade-in-image';
+import React, { Component } from 'react';
+import { Image, Platform, Text, TouchableOpacity, View } from 'react-native';
+import FadeIn from '@expo/react-native-fade-in-image';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { connect } from 'react-redux';
 
@@ -17,7 +12,6 @@ import styles from './styles/CardThree';
 const iconStar = <Icon name="md-star" size={16} color="#F5B642" />;
 
 class CardThree extends Component {
-
   constructor(props) {
     super(props);
   }
@@ -26,24 +20,37 @@ class CardThree extends Component {
     const { info, viewMovie } = this.props;
     return (
       <View style={styles.cardContainer}>
-        <TouchableOpacity activeOpacity={0.9} onPress={viewMovie.bind(this, info.id)}>
+        <TouchableOpacity
+          activeOpacity={0.9}
+          onPress={viewMovie.bind(this, info.id)}
+        >
           <View style={styles.card}>
-            <FadeIn placeholderStyle={{backgroundColor: Platform.OS === 'android' ? 'transparent' : '#eee'}}>
-              <Image source={{ uri: `${TMDB_IMG_URL}/w185/${info.poster_path}` }} style={styles.cardImage} />
+            <FadeIn
+              placeholderStyle={{
+                backgroundColor:
+                  Platform.OS === 'android' ? 'transparent' : '#eee'
+              }}
+            >
+              <Image
+                source={{ uri: `${TMDB_IMG_URL}/w185/${info.poster_path}` }}
+                style={styles.cardImage}
+              />
             </FadeIn>
             <View style={styles.cardDetails}>
-              <Text
-                style={styles.cardTitle}
-                numberOfLines={3}>
+              <Text style={styles.cardTitle} numberOfLines={3}>
                 {info.original_title}
               </Text>
               <View style={styles.cardGenre}>
-                <Text style={styles.cardGenreItem}>{info.release_date.substring(0, 4)}</Text>
+                <Text style={styles.cardGenreItem}>
+                  {info.release_date.substring(0, 4)}
+                </Text>
               </View>
               <View style={styles.cardNumbers}>
                 <View style={styles.cardStar}>
                   {iconStar}
-                  <Text style={styles.cardStarRatings}>{info.vote_average.toFixed(1)}</Text>
+                  <Text style={styles.cardStarRatings}>
+                    {info.vote_average.toFixed(1)}
+                  </Text>
                 </View>
                 <Text style={styles.cardRunningHours} />
               </View>
