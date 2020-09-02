598f97304cb9c5d158e796908d99a37c1f7b1ec4
igserdar <63535330+igserdar@users.noreply.github.com>
2020-09-02 17:38:07 +0300

    Update CloseButton.js

--- navigation/CloseButton.js
+++ navigation/CloseButton.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { withNavigation } from '@exponent/ex-navigation';
-import {
-  TouchableOpacity,
-  Text,
-} from 'react-native';
+import { withNavigation } from '@expo/ex-navigation';
+import { TouchableOpacity, Text } from 'react-native';
 
 @withNavigation
 export default class CloseButton extends React.Component {
   render() {
     return (
-      <TouchableOpacity onPress={() => this.props.navigation.dismissModal()} style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingRight: 15,
-      }}>
-        <Text style={{color: '#fff', fontSize: 16,}}>Close</Text>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.dismissModal()}
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingRight: 15
+        }}
+      >
+        <Text style={{ color: '#fff', fontSize: 16 }}>Close</Text>
       </TouchableOpacity>
     );
   }
